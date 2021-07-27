7a6178fc5dfde502ecbe273b1ae9d8b19de7ae3d
humyrx <humyra.HiC@gmail.com>
2021-07-27 07:18:19 -0400

    Code for A9 complete
    The code for A9 is complete and works accurately with test_data.csv. All that needs to be done is incorporation of diabetes.csv and how to split of that file for training and testing.

--- A9/a9_chowdhuryh2016.py
+++ A9/a9_chowdhuryh2016.py
@@ -1,11 +1,12 @@
 import csv
 
 
+# Read data set from specified file
+# Return list of tuples in format: patient id, diagnosis, 8 attributes
 def make_data_set(file_name):
-    # read data set from specified file
-    # return list of tuples in format: 8 attributes, diagnosis
     input_set_list = []
     fields = [] # intialize the titles
+    patient_id = 0
 
     # read csv file
     with open(file_name, 'r') as inputfile:
@@ -14,25 +15,26 @@ def make_data_set(file_name):
 
         # extract each data row 
         for row in inputfile_reader:
-            # get each attribute and diagnosis for a patient and create a tuple
-            patient_tuple = int(row[0]), int(row[1]), int(row[2]), int(row[3]), int(row[4]), \
-                float(row[5]), float(row[6]), int(row[7]), int(row[8])
-            # print("patient_tuple ", patient_tuple)
+            # get each diagnosis and attribute for a patient and create a tuple
+            patient_id += 1
+            patient_tuple = patient_id, int(row[8]), int(row[0]), int(row[1]), int(row[2]), int(row[3]), \
+                int(row[4]), float(row[5]), float(row[6]), int(row[7])
+            print("patient_tuple ", patient_tuple)
             input_set_list.append(patient_tuple) # append tuple to list
-        # print(input_set_list)
+        # print("input_set_list ", input_set_list)
         
     return input_set_list
 
 
-# element-by-element sums of two lists of 7 items
+# Element-by-element sums of two lists of 7 items
 def sum_lists(list1, list2):
     sums_list = []
-    for index in range(7):
+    for index in range(8):
         sums_list.append(list1[index] + list2[index])
     return sums_list
 
 
-# convert each list element into an average by dividing by the total
+# Convert each list element into an average by dividing by the total
 def make_averages(sums_list, total_int):
     averages_list = []
     for value_int in sums_list:
@@ -40,7 +42,7 @@ def make_averages(sums_list, total_int):
     return averages_list
 
 
-# build a classifier using the training set
+# Build a classifier using the training set
 def train_classifier(training_set_list):
     nondiabetic_sums_list = [0] * 8 # list of sums of nondiabetic attributes
     nondiabetic_count = 0 # count of nondiabetic patients
@@ -48,59 +50,69 @@ def train_classifier(training_set_list):
     diabetic_count = 0 # count of diabetic patients
 
     for patient_tuple in training_set_list:
-        if patient_tuple[8] == 0: # if nondiabetic diagnosis
+        if patient_tuple[1] == 0: # if nondiabetic diagnosis
             # add nondiabetic attributes to nondiabetic total
-            nondiabetic_sums_list = sum_lists(nondiabetic_sums_list, patient_tuple[0:8])
+            nondiabetic_sums_list = sum_lists(nondiabetic_sums_list, patient_tuple[2:])
             nondiabetic_count += 1
         else: # else diabetic diagnosis
             # add diabetic attributes to diabetic total
-            diabetic_sums_list = sum_lists(diabetic_sums_list, patient_tuple[0:8])
+            diabetic_sums_list = sum_lists(diabetic_sums_list, patient_tuple[2:])
             diabetic_count += 1
 
-    # print("nondiabetic_sums_list ", nondiabetic_sums_list, "\ndiabetic_sums_list ", diabetic_sums_list)
+    print("nondiabetic_sums_list ", nondiabetic_sums_list, "\ndiabetic_sums_list ", diabetic_sums_list)
     # find averages of each set of nondiabetic or diabetic attributes
     nondiabetic_averages_list = make_averages(nondiabetic_sums_list, nondiabetic_count)
     diabetic_averages_list = make_averages(diabetic_sums_list, diabetic_count)
-    # print("nondiabetic_averages_list ", nondiabetic_averages_list, "\ndiabetic_averages_list ", diabetic_averages_list)
+    print("nondiabetic_averages_list ", nondiabetic_averages_list, "\ndiabetic_averages_list ", diabetic_averages_list)
     # seperator values for each attribute averages nondiabetic and diabetic
     classifier_list = make_averages(sum_lists(nondiabetic_averages_list, diabetic_averages_list), 2)
-    # print("classifier_list ", classifier_list)
+    print("classifier_list ", classifier_list)
     return classifier_list
 
 
+# Given test set and classifier, classify each patient in test set
+# Return list of result tuples: (id, nondiabetic_count, diabetic_count, diagnosis)
 def classify_test_set(test_set_list, classifier_list):
     result_list = []
+    # for each patient
     for patient_tuple in test_set_list:
-        benign_count = 0
-        malignant_count = 0
+        nondiabetic_count = 0
+        diabetic_count = 0
         id_str, diagnosis_str = patient_tuple[:2]
-        for index in range(9):
+        # for each attribute of the patient
+        for index in range(8):
+            # if actual patient attributes is greater than separator value
+            # Note: the patient tuple has two extra elements at the beginning 
+            # so we add 2 to each patient index to only index attributes
             if patient_tuple[index + 2] > classifier_list[index]:
-                malignant_count += 1
+                diabetic_count += 1
             else:
-                benign_count += 1
-        print(diagnosis_str)
-        result_tuple = (id_str, benign_count, malignant_count, diagnosis_str)
+                nondiabetic_count += 1
+        result_tuple = (id_str, nondiabetic_count, diabetic_count, diagnosis_str)
+        print("result_tuple ", result_tuple)
         result_list.append(result_tuple)
     return result_list
 
 
+# Check results and report count of inaccurate classifications
 def report_results(result_list):
     total_count = 0
     inaccurate_count = 0
     for result_tuple in result_list:
-        benign_count, malignant_count, diagnosis_str = result_tuple[1:4]
+        nondiabetic_count, diabetic_count, diagnosis_str = result_tuple[1:4]
+        print("result_tuple[1:4] ", result_tuple[1:4])
         total_count += 1
-        if (benign_count > malignant_count) and (diagnosis_str == "m"):
+        if (nondiabetic_count > diabetic_count) and (diagnosis_str == 1):
+            # Wrong classification
             inaccurate_count += 1
-        elif diagnosis_str == "b":
+        elif (nondiabetic_count < diabetic_count) and (diagnosis_str == 0):
+            # Wrong classification
             inaccurate_count += 1
-    print(
-        "Of ", total_count, " patients, there were ", inaccurate_count, " inaccuracies"
-    )
+    print("Of ", total_count, " patients, there were ", inaccurate_count, " inaccuracies")
 
 
 def main():
+    print()
     print("Reading in training data...")
     training_file = "test_data.csv"
     training_set_list = make_data_set(training_file)
@@ -111,16 +123,16 @@ def main():
     classifier_list = train_classifier(training_set_list)
     print("Done training classifier. \n")
 
-#   print("Reading in test data...")
-#   test_file = "test_data.txt"
-#   test_set_list = make_data_set(test_file)
-#   print("Done reading test data. \n")
+    print("Reading in test data...")
+    test_file = "test_data.csv"
+    test_set_list = make_data_set(test_file)
+    print("Done reading test data. \n")
 
-#   print("Classifying records...")
-#   result_list = classify_test_set(test_set_list, classifier_list)
-#   print("Done classifying. \n")
+    print("Classifying records...")
+    result_list = classify_test_set(test_set_list, classifier_list)
+    print("Done classifying. \n")
 
-#   report_results(result_list)
-#   print("Program finished.")
+    report_results(result_list)
+    print("Program finished.")
 
 main()
