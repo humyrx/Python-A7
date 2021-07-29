9994ab22fc286cbd66553ce5e6b0394aae36431c
humyrx <humyra.HiC@gmail.com>
2021-07-29 01:37:35 -0400

    User can choose which file(s) to upload

--- A9/a9_chowdhuryh2016.py
+++ A9/a9_chowdhuryh2016.py
@@ -1,26 +1,30 @@
 import csv
+# from google.colab import files
 
 
 # Read data set from specified file
 # Return list of tuples in format: patient id, diagnosis, 8 attributes
 def make_data_set(file_name):
     input_set_list = []
-    fields = [] # intialize the titles
+    fields = []  # intialize the titles
+    patient_id = 0
 
     # read csv file
     with open(file_name, 'r') as inputfile:
-        inputfile_reader = csv.reader(inputfile) # create a csv reader object
-        fields = next(inputfile_reader) # extract field names through the first row
+        inputfile_reader = csv.reader(inputfile)  # create a csv reader object
+        # extract field names through the first row
+        fields = next(inputfile_reader)
 
-        # extract each data row 
+        # extract each data row
         for row in inputfile_reader:
             # get each diagnosis and attribute for a patient and create a tuple
-            patient_tuple = int(row[0]), int(row[9]), int(row[1]), int(row[2]), int(row[3]), \
-                int(row[4]), int(row[5]), float(row[6]), float(row[7]), int(row[8])
+            patient_id += 1
+            patient_tuple = patient_id, int(row[8]), int(row[0]), int(row[1]), int(row[2]), int(row[3]), \
+                int(row[4]), float(row[5]), float(row[6]), int(row[7])
             # print("patient_tuple ", patient_tuple)
-            input_set_list.append(patient_tuple) # append tuple to list
+            input_set_list.append(patient_tuple)  # append tuple to list
         # print("input_set_list ", input_set_list)
-        
+
     return input_set_list
 
 
@@ -42,28 +46,32 @@ def make_averages(sums_list, total_int):
 
 # Build a classifier using the training set
 def train_classifier(training_set_list):
-    nondiabetic_sums_list = [0] * 8 # list of sums of nondiabetic attributes
-    nondiabetic_count = 0 # count of nondiabetic patients
-    diabetic_sums_list = [0] * 8 # list of sums of diabetic attributes
-    diabetic_count = 0 # count of diabetic patients
+    nondiabetic_sums_list = [0] * 8  # list of sums of nondiabetic attributes
+    nondiabetic_count = 0  # count of nondiabetic patients
+    diabetic_sums_list = [0] * 8  # list of sums of diabetic attributes
+    diabetic_count = 0  # count of diabetic patients
 
     for patient_tuple in training_set_list:
-        if patient_tuple[1] == 0: # if nondiabetic diagnosis
+        if patient_tuple[1] == 0:  # if nondiabetic diagnosis
             # add nondiabetic attributes to nondiabetic total
-            nondiabetic_sums_list = sum_lists(nondiabetic_sums_list, patient_tuple[2:])
+            nondiabetic_sums_list = sum_lists(
+                nondiabetic_sums_list, patient_tuple[2:])
             nondiabetic_count += 1
-        else: # else diabetic diagnosis
+        else:  # else diabetic diagnosis
             # add diabetic attributes to diabetic total
-            diabetic_sums_list = sum_lists(diabetic_sums_list, patient_tuple[2:])
+            diabetic_sums_list = sum_lists(
+                diabetic_sums_list, patient_tuple[2:])
             diabetic_count += 1
 
     # print("nondiabetic_sums_list ", nondiabetic_sums_list, "\ndiabetic_sums_list ", diabetic_sums_list)
     # find averages of each set of nondiabetic or diabetic attributes
-    nondiabetic_averages_list = make_averages(nondiabetic_sums_list, nondiabetic_count)
+    nondiabetic_averages_list = make_averages(
+        nondiabetic_sums_list, nondiabetic_count)
     diabetic_averages_list = make_averages(diabetic_sums_list, diabetic_count)
     # print("nondiabetic_averages_list ", nondiabetic_averages_list, "\ndiabetic_averages_list ", diabetic_averages_list)
     # seperator values for each attribute averages nondiabetic and diabetic
-    classifier_list = make_averages(sum_lists(nondiabetic_averages_list, diabetic_averages_list), 2)
+    classifier_list = make_averages(
+        sum_lists(nondiabetic_averages_list, diabetic_averages_list), 2)
     # print("classifier_list ", classifier_list)
     return classifier_list
 
@@ -80,13 +88,14 @@ def classify_test_set(test_set_list, classifier_list):
         # for each attribute of the patient
         for index in range(8):
             # if actual patient attributes is greater than separator value
-            # Note: the patient tuple has two extra elements at the beginning 
+            # Note: the patient tuple has two extra elements at the beginning
             # so we add 2 to each patient index to only index attributes
             if patient_tuple[index + 2] > classifier_list[index]:
                 diabetic_count += 1
             else:
                 nondiabetic_count += 1
-        result_tuple = (id_str, nondiabetic_count, diabetic_count, diagnosis_str)
+        result_tuple = (id_str, nondiabetic_count,
+                        diabetic_count, diagnosis_str)
         # print("result_tuple ", result_tuple)
         result_list.append(result_tuple)
     return result_list
@@ -108,25 +117,56 @@ def report_results(result_list):
             # Wrong classification
             inaccurate_count += 1
             false_positive += 1
-    print("Of", total_count, "patients, there were", inaccurate_count, "inaccuracies")
-    print("There were", false_negative, "false negatives and", false_positive, "false positives")
-    print("The percent accuracy is", ((total_count-inaccurate_count)/total_count)*100, "%")
+    print("Of", total_count, "patients, there were",
+          inaccurate_count, "inaccuracies")
+    print("There were", false_negative, "false negatives and",
+          false_positive, "false positives")
+    print("The percent accuracy is",
+          ((total_count-inaccurate_count)/total_count)*100, "%")
 
 
 def main():
     print()
-    print("Reading in training data...")
-    training_file = "training_data.csv"
+
+    print("Welcome! In this program, we will be interpreting the Pima Indians diabetes dataset using a classifer that we built.\n")
+    option = "Z"
+
+    while option.upper() != "G" and option.upper() != "D":
+        print("Which csv files(s) would you like to upload?")
+        print("(G) Upload the two files in the following GitHub repo as the training and testing file")
+        print("Link to GitHub repo: https://github.com/humyrx/A9-Diabetes-Files")
+        print("Note: the diabetes.csv file was split 80:20 to create these two files. The training file has more data than the testing file")
+        print("(D) Upload just the diabetes.csv file as the training and testing file")
+        option = input("Answer: ")
+        if option.upper() != "G" and option.upper() != "D":
+            print("Invalid answer. Please enter either (G) or (D).\n")
+
+    if option.upper() == "G":
+        print("You chose (G) GitHub files.")
+        print("Please enter the training_data.csv file.")
+        # uploaded = files.upload()
+        training_file = "training_data.csv"
+        print("You chose (D) Diabetes.csv file.")
+        print("Please enter the test_data.csv file.")
+        # uploaded = files.upload()
+        test_file = "test_data.csv"
+    else:
+        print("Please enter the diabetes.csv file.")
+        # uploaded = files.upload()
+        training_file = "diabetes.csv"
+        test_file = "diabetes.csv"
+
+    print("\nReading in training data...")
+    # training_file = "training_data.csv"
     training_set_list = make_data_set(training_file)
     print("Done reading training data. \n")
-
 
     print("Training classifier...")
     classifier_list = train_classifier(training_set_list)
     print("Done training classifier. \n")
 
     print("Reading in test data...")
-    test_file = "test_data.csv"
+    # test_file = "test_data.csv"
     test_set_list = make_data_set(test_file)
     print("Done reading test data. \n")
 
@@ -136,5 +176,6 @@ def main():
 
     report_results(result_list)
     print("\nProgram finished.\n")
+
 
 main()
